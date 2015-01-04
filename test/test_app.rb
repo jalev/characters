a92c6b72b6a9bd39ec02d8e5ebafac76c7c1dcdc
James Bach <qweet.ing@gmail.com>
2015-01-04 19:04:06 +0000

    Restructure tests

--- test/test_app.rb
+++ test/test_app.rb
@@ -7,12 +7,37 @@ class TestApp< Minitest::Test
     Sinatra::Application
   end 
 
-  context '/get' do
-    should 'displays main page' do 
-      get '/'
-      assert last_response.ok?
+  context 'API::V1::Character' do
+    context 'errors' do  
+      should 'when environment has no character set' do 
+        assert_raises(RuntimeError){
+          get '/character'
+        }
+      end
+    end 
+
+    context 'request' do 
+      setup do 
+        Sinatra::Application.set :character => "a"
+      end 
+
+      context '/character' do 
+        should 'GET /character' do 
+          get '/character/'
+          assert last_response.ok?
+          assert last_response.body.include?('character')
+        end
+
+      end 
+
+      context '/info' do 
+        should 'GET /info about current process' do 
+          get '/info'
+          assert last_response.ok?
+          assert last_response.body.include?('host')
+        end 
+      end 
     end 
   end 
-
 end
 
