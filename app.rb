2f2ab6326cdf8be69ff007478fc7ea0939f45579
James Bach <qweet.ing@gmail.com>
2015-01-04 19:52:51 +0000

    Add '/character' route

--- app.rb
+++ app.rb
@@ -1,17 +1,15 @@
 require 'rubygems'
 require 'sinatra'
-require 'etcd'
-
-set :logging, true
+require 'json'
 
 configure do
-  set :etcd_host => ENV.has_key?('ETCD_HOST') ? ENV['ETCD_HOST'] : "localhost"
-  set :etcd_port => ENV.has_key?('ETCD_PORT') ? ENV['ETCD_PORT'] : "4001"
-  set :etcd_path => ENV.has_key?('ETCD_PATH') ? ENV['ETCD_PATH'] : "/service"
+    set :logging, true
+    set :character => ENV.has_key?('CHARACTER') ? ENV['CHARACTER'] : nil
 end
 
-client = Etcd.client(:host => settings.etcd_host, :port => settings.etcd_port)
+get '/character' do
 
-get '/' do
+  halt 500, {:error => "Character environment variable must be set!"}.to_json if settings.character.nil?
+  return {:character => settings.character }.to_json
+
 end
-
